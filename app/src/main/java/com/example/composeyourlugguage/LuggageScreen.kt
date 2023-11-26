fb69068429258b3b40b04102d471c4ff813addb2
valecarrirolo <valentina.carrirolo@gmail.com>
2023-11-26 22:33:33 +0100

    Title with 50 elements LazyColumn

---
+++ app/src/main/java/com/example/composeyourlugguage/LuggageScreen.kt
@@ -0,0 +1,30 @@
+package com.example.composeyourlugguage
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+// comp+invio per creare Composable Fun
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun LuggageScreen() {
+    Scaffold(
+        topBar = {
+            MediumTopAppBar(
+                title = { Text(text = "Compose Your Luggage") },
+            )
+        },
+    ) { innerPadding ->
+        LazyColumn(Modifier.padding(innerPadding).fillMaxSize()) {
+            items(50) {
+                Text(text = "Item $it")
+            }
+        }
+    }
+}
